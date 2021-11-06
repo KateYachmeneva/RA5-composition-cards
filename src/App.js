cf6a0b6479af2bb1811056452c9a61b538c39b01
Ekaterina Iachmeneva <iachmenevaev@mail.ru>
2021-11-07 01:20:38 +0300

    almost ready

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Card from "./Components/Card/Card";
+import CardTitle from "./Components/Card/CardTitle/CardTitle";
+import CardText from "./Components/Card/CardText/CardText";
+import Button from "./Components/Button/Button";
+
+const URL = "https://avatars.mds.yandex.net/get-kinopoisk-image/1629390/54eab538-be45-4d58-bf5d-c83f86cb252c/3840x";
+const URLTWO = "https://avatars.mds.yandex.net/get-kinopoisk-image/1946459/89c43dde-6bb9-4c1d-8e2c-59c3af5430d6/800x800";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container pt-5 d-flex">
+      <Card imageURL={URL} 
+            style={{ width: "20rem", marginRight: "50px" }}
+            >
+        <CardTitle>Анатомия страсти: Вторая команда </CardTitle>
+        <CardText>
+          Сериал рассказывает о молодых врачах, делающих свои первые шаги в
+          больнице «Грей-Слоан Мемориальный госпиталь».
+        </CardText>
+        <Button>Подробнее</Button>
+      </Card>
+      <Card style={{width: '20rem'}}
+            imageURL={URLTWO}>
+      <CardTitle>Звёздные войны. Эпизод I: Скрытая угроза</CardTitle>
+      <CardText>Неспокойные времена настали для Галактической Республики. Налогообложение торговых путей к отдаленным
+        солнечным системам стало причиной раздоров.</CardText>
+      <Button>Узнать больше</Button>
+    </Card>
     </div>
   );
 }
 
 export default App;
+
+console.log(App);
